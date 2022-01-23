662dac6c950d721ef88f3602d794fb84c11c90d7
Otavio Ramos <otavio.santana@portaldedocumentos.com.br>
2022-01-23 20:25:09 -0300

    MainHeader component part 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-import NewPlace from './places/pages/NewPlace';
-import Users from './user/pages/Users';
+import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import Users from "./user/pages/Users";
 
-const App= () => {
+const App = () => {
   return (
-  <Router>
-    <Switch>
-    <Route path="/" exact>
-      <Users />
-    </Route>
-    <Route path="/places/new" exact>
-      <NewPlace />
-    </Route>
-    <Redirect to="/" />
-    </Switch>
-  </Router>);
-}
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
+};
 
 export default App;
