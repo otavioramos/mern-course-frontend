1b513542190e3ac203361304317ad6906ce5226e
Otavio Ramos <otavio.santana@portaldedocumentos.com.br>
2022-01-23 19:17:51 -0300

    Users page and UsersList component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-    return <h2>Users Works!</h2>;
-}
+  const USERS = [
+    {
+      id: "u1",
+      name: "Otavio",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/e/e8/Lc3_2018_%28263682303%29_%28cropped%29.jpeg",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
+};
 
 export default Users;
